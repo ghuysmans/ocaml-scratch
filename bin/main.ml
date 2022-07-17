459dc906874ea4ae94cd414234d7ac59b2485a7e
Guillaume Huysmans <ghuysmans99@gmail.com>
2022-07-17 04:20:54 +0200

    List sprites, stages and blocks

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,26 @@
+open Scratch
+
 let () =
-  match Yojson.Safe.from_channel stdin |> Scratch.Project.of_yojson with
+  match Yojson.Safe.from_channel stdin |> Project.of_yojson with
   | Error e -> prerr_endline e
-  | Ok _ -> print_endline "ok"
+  | Ok {targets; meta; _} ->
+    Printf.printf "Version: %s\nUser-Agent: %s\n"
+      meta.creator_vm meta.last_user_agent;
+    List.iter (fun t ->
+      let basic =
+        match t with
+        | Target.Sprite (t, s) ->
+          Printf.printf "Sprite: %s%s @ (%f,%f)\n"
+            t.name (if s.visible then "" else "*")
+            s.x s.y;
+          t
+        | Stage (t, _) ->
+          Printf.printf "Stage: %s\n" t.name;
+          t
+      in
+      List.iter (fun (_, x) ->
+        match x with
+        | Block.Full f -> Printf.printf "* %s\n" f.opcode
+        | _ -> ()
+      ) basic.blocks
+    ) targets
