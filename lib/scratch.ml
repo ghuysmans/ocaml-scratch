0638057a7f08a7d08da0bf3b46a7a626cc0608f1
Guillaume Huysmans <ghuysmans99@gmail.com>
2022-07-17 03:27:13 +0200

    Parse target as either sprite or stage

--- lib/scratch.ml
+++ lib/scratch.ml
@@ -255,49 +255,72 @@ module Sound = struct
     Yojson.Safe.Util.combine (asset_to_yojson a) (s_to_yojson s)
 end
 
-type target = {
-  is_stage: bool [@key "isStage"];
-  name: string;
-  variables: variable map;
-  lists: lst map;
-  broadcasts: string map;
-  blocks: block map;
-  comments: comment map;
-  current_costume: int [@key "currentCostume"];
-  costumes: Costume.t list;
-  sounds: Sound.t list;
-  layer_order: int [@key "layerOrder"];
-  volume: int; (** between 0 and 100 *)
-} [@@deriving yojson {strict=false (* FIXME? *)}]
+module Target = struct
+  type target = {
+    is_stage: bool [@key "isStage"];
+    name: string;
+    variables: variable map;
+    lists: lst map;
+    broadcasts: string map;
+    blocks: block map;
+    comments: comment map;
+    current_costume: int [@key "currentCostume"];
+    costumes: Costume.t list;
+    sounds: Sound.t list;
+    layer_order: int [@key "layerOrder"];
+    volume: int; (** between 0 and 100 *)
+  } [@@deriving yojson {meta=true}]
 
-type video_state =
-  | On [@name "on"]
-  | Off [@name "off"]
-  | On_flipped [@name "on-flipped"]
-  [@@deriving yojson]
+  type video_state =
+    | On [@name "on"]
+    | Off [@name "off"]
+    | On_flipped [@name "on-flipped"]
+    [@@deriving yojson]
 
-type stage = {
-  tempo: int; (** in BPM *)
-  video_state: video_state tag [@key "videoState"];
-  video_transparency: int [@key "videoTransparency"];
-  tts_language: string [@key "textToSpeechLanguage"];
-} [@@deriving yojson {strict=false (* FIXME target *)}]
+  type stage = {
+    tempo: int; (** in BPM *)
+    video_state: video_state tag [@key "videoState"];
+    video_transparency: int [@key "videoTransparency"];
+    tts_language: string option [@key "textToSpeechLanguage"];
+  } [@@deriving yojson]
 
-type rotation_style =
-  | All_around [@name "all around"]
-  | Left_right [@name "left-right"]
-  | Don't_rotate [@name "don't rotate"]
-  [@@deriving yojson]
+  type rotation_style =
+    | All_around [@name "all around"]
+    | Left_right [@name "left-right"]
+    | Don't_rotate [@name "don't rotate"]
+    [@@deriving yojson]
 
-type sprite = {
-  visible: bool;
-  x: int;
-  y: int;
-  size: int; (** in percent *)
-  direction: float;
-  draggable: bool;
-  rotation_style: rotation_style tag [@key "rotationStyle"];
-} [@@deriving yojson {strict=false (* FIXME target *)}]
+  type sprite = {
+    visible: bool;
+    x: float;
+    y: float;
+    size: float; (** in percent *)
+    direction: float;
+    draggable: bool;
+    rotation_style: rotation_style tag [@key "rotationStyle"];
+  } [@@deriving yojson]
+
+  type t =
+    | Sprite of target * sprite
+    | Stage of target * stage
+
+  let of_yojson = function
+    | `Assoc l ->
+      let t, s = List.(partition (fun (x, _) -> mem x Yojson_meta_target.keys) l) in
+      Result.bind (target_of_yojson (`Assoc t)) (fun t ->
+        if t.is_stage then
+          Result.map (fun s -> Stage (t, s)) (stage_of_yojson (`Assoc s))
+        else
+          Result.map (fun s -> Sprite (t, s)) (sprite_of_yojson (`Assoc s))
+      )
+    | _ -> Error "Target.of_yojson"
+
+  let to_yojson x =
+    let combine = Yojson.Safe.Util.combine in
+    match x with
+    | Sprite (a, s) -> combine (target_to_yojson a) (sprite_to_yojson s)
+    | Stage (a, s) -> combine (target_to_yojson a) (stage_to_yojson s)
+end
 
 type mode =
   | Default [@name "default"]
@@ -344,7 +367,7 @@ type meta = {
 } [@@deriving yojson]
 
 type t = {
-  targets: target list;
+  targets: Target.t list;
   monitors: monitor list;
   extensions: extension tag list;
   meta: meta;
